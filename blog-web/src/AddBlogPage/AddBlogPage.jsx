1ec103161ec519c770bf5f93f8fd3c3562a22f2c
ajithkumar-oos <ajithkumar2004ganesh@gmail.com>
2026-02-13 16:30:12 +0530

    Update API_BASE URL to production endpoint

--- blog-web/src/AddBlogPage/AddBlogPage.jsx
+++ blog-web/src/AddBlogPage/AddBlogPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://personal-blog-web-backend.onrender.com";
 
 function BlogCards() {
   const [posts, setPosts] = useState([]);
