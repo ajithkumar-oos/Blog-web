96ea8c80733aede8a8765ab9e4ac1dc518784100
ajithkumar-oos <ajithkumar2004ganesh@gmail.com>
2026-02-13 16:32:41 +0530

    Update registration API endpoint to production URL

--- blog-web/src/register/Register.jsx
+++ blog-web/src/register/Register.jsx
@@ -17,7 +17,7 @@ function Register() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://personal-blog-web-backend.onrender.com",
         data
       );
 
